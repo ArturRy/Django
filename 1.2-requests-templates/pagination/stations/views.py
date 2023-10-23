3d1bd734a86fbfb29da8a8d0c0f455c78f208221
Artur <mini--me@mail.ru>
2023-10-23 22:48:34 +0300

    second HW

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -14,21 +14,23 @@ def index(request):
 
 
 
+
 with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csv_file:
     reader = csv.DictReader(csv_file)
-    info = []
-    for row in reader:
-        info.append([row['Name'], row['Street'], row['District']])
+    CONTENT = [(row['Name'], row['Street'], row['District']) for row in reader]
 
-CONTENT = [(i) for i in info]
+
+
 def bus_stations(request):
-    paginator = Paginator(CONTENT, 10)
-    page = paginator.get_page(1)
+
+    page_number = int(request.GET.get("page", 1))
+    paginator = Paginator(str(CONTENT), 10)
+    page = paginator.get_page(page_number)
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
     context = {
-        'bus_stations': paginator,
+        'bus_stations': page.object_list,
         'page': page,
     }
     return render(request, 'stations/index.html', context)
