346cfe0eeec495b5b9c190d69686b68251bf9ad1
Artur <mini--me@mail.ru>
2023-10-24 19:16:03 +0300

    Second HW

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,34 +1,29 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-import  csv
+import csv
+
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
+def cont_reader():
+    result = []
+    with open('data-398-2018-08-30.csv', newline='', encoding='UTF-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for line in reader:
+            result.append(line)
+    return result
 
 
-
-
-
-
-
-
-with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file)
-    CONTENT = [(row['Name'], row['Street'], row['District']) for row in reader]
-
+CONTENT = cont_reader()
 
 
 def bus_stations(request):
-
     page_number = int(request.GET.get("page", 1))
-    paginator = Paginator(str(CONTENT), 10)
+    paginator = Paginator(CONTENT, 10)
     page = paginator.get_page(page_number)
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
     context = {
         'bus_stations': page.object_list,
         'page': page,
--- 1.2-requests-templates/pagination/решалка.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as csv_file:
-    reader = csv.DictReader(csv_file)
-
-    CONTENT = [(row['Name'], row['Street'], row['District']) for row in reader]
-print(str(CONTENT))
