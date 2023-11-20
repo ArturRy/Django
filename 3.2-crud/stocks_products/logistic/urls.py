034277732258d7d1fd1b5f66dbbdf673eda53ce1
Artur <mini--me@mail.ru>
2023-11-20 22:54:50 +0300

    commit

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet
+from .views import ProductViewSet, StockViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('stocks', StockViewSet)
+router.register("products", ProductViewSet)
+router.register("stocks", StockViewSet)
 
 urlpatterns = router.urls
